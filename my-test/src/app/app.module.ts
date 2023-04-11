6ad9f98e5729f6fec1b8ddc874aa5363364b658e
syed.yarooq <yarooq.hussain@dastgyr.com>
2023-04-11 20:53:22 +0500

    initial commit

---
+++ my-test/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+
+@NgModule({
+  declarations: [
+    BrowserModule,
+    HttpClientModule,
+    AppComponent,
+    PokemonListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
