8e98ec45b08e2a1c10caf6f4b68505218a1b3889
syed.yarooq <yarooq.hussain@dastgyr.com>
2023-04-11 20:53:39 +0500

    file left

--- my-test/src/app/app.module.ts
+++ my-test/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+/* import { HttpClientModule } from '@angular/common/http';
+ */
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
-    BrowserModule,
-    HttpClientModule,
+  /*   BrowserModule,
+    HttpClientModule, */
     AppComponent,
     PokemonListComponent
   ],
